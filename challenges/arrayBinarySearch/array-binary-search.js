e5c999d29da3aaa04292e626b0077ee859f74cdb
sanvean74 <dlambertpdx@gmail.com>
2019-09-22 16:45:23 -0700

    adding binary-search image

--- challenges/arrayBinarySearch/array-binary-search.js
+++ challenges/arrayBinarySearch/array-binary-search.js
@@ -3,6 +3,7 @@ function binarySearch(arr, search) {
   let end = arr.length - 1;
   while(start <= end) {
     let mid = Math.floor((start + end) / 2);
+    console.log(mid);
     if(search === arr[mid]) {
       return mid;
     } else if(search < arr[mid]) {
