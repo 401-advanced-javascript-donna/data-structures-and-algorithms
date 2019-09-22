0e7c4b17d5b60e31488cd5af1b331794828e2d5b
sanvean74 <dlambertpdx@gmail.com>
2019-09-22 14:13:23 -0700

    binary search and test working

---
+++ challenges/arrayBinarySearch/array-binary-search.js
@@ -0,0 +1,16 @@
+function binarySearch(arr, search) {
+  let start = 0; 
+  let end = arr.length - 1;
+  while(start <= end) {
+    let mid = Math.floor((start + end) / 2);
+    if(search === arr[mid]) {
+      return mid;
+    } else if(search < arr[mid]) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+  return -1;
+}
+module.exports = binarySearch;
