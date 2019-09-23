47fd623732cf826b66ebba067594d9d5d7534916
sanvean74 <dlambertpdx@gmail.com>
2019-09-22 17:30:24 -0700

    add array shift image

--- challenges/arrayShift/array-shift.js
+++ challenges/arrayShift/array-shift.js
@@ -1,9 +1,10 @@
 const insertShiftArray = (arr, val) => {
   for(let i = arr.length - 1; i >= 0; i--) {
     arr[i + 1] = arr[i];
+    console.log(arr[i]);
   }
   arr[0] = val;
   return arr;
-}
+};
 
 module.exports = insertShiftArray;
